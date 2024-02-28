3327b91f2a1c84eed930c010a4e37af7ad196276
Richard Hawkins <hurricanerix@gmail.com>
2024-02-28 08:31:09 -0600

    Initial Commit

---
+++ GitHubCICDSandbox/GitHubCICDSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCICDSandboxApp.swift
+//  GitHubCICDSandbox
+//
+//  Created by Richard Hawkins on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCICDSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
